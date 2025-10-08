9ac13a28ffcc83390bce218d96bddb9d622367fc
Gerrard Rich Zainal <gerrardrichzainal808@gmail.com>
2025-10-08 19:46:26 +0800

    Third Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,104 @@
 import 'package:flutter/material.dart';
+import 'package:uni_links/uni_links.dart';
+import 'dart:async';
+import 'detail_screen.dart';
+import 'profile_screen.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Deep Link Demo',
-      initialRoute: '/',
-      routes: {
-        '/': (context) => HomeScreen(),
-        '/details': (context) => DetailScreen(),
-      },
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const HomeScreen(),
     );
   }
 }
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  StreamSubscription? _sub;
+
+  @override
+  void initState() {
+    super.initState();
+    _handleIncomingLinks();
+  }
+
+  void _handleIncomingLinks() {
+    _sub = uriLinkStream.listen((Uri? uri) {
+      if (uri != null) {
+        _navigateFromLink(uri);
+      }
+    }, onError: (err) {
+      print('Error parsing incoming link: $err');
+    });
+  }
+
+  void _navigateFromLink(Uri uri) {
+    if (uri.host == 'details' && uri.pathSegments.isNotEmpty) {
+      final id = uri.pathSegments[0];
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => DetailsScreen(id: id)),
+      );
+    } else if (uri.host == 'profile' && uri.pathSegments.isNotEmpty) {
+      final username = uri.pathSegments[0];
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => ProfileScreen(username: username)),
+      );
+    }
+  }
+
+  @override
+  void dispose() {
+    _sub?.cancel();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Home')),
+      appBar: AppBar(title: const Text('Home Screen')),
       body: Center(
-        child: Text('Welcome! Try opening myapp://details/42'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const DetailsScreen(id: '1')),
+                );
+              },
+              child: const Text('Open Details'),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const ProfileScreen(username: 'alex')),
+                );
+              },
+              child: const Text('Go to Profile'),
+            ),
+          ],
+        ),
       ),
-    );
-  }
-}
-
-class DetailScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Details')),
-      body: Center(child: Text('You are on the Detail screen!')),
     );
   }
 }
---
+++ lib/profile_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ProfileScreen extends StatelessWidget {
+  final String username;
+  const ProfileScreen({super.key, required this.username});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Profile')),
+      body: Center(
+        child: Text('Hello, $username!'),
+      ),
+    );
+  }
+}
